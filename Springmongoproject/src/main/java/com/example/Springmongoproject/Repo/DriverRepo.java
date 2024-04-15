2592167c4d2b034aceff5c1bb38bdb104beca6d4
VASANTH S <122204153+VASANTH3105@users.noreply.github.com>
2024-04-15 10:05:41 +0530

    Delete Springmongoproject directory

--- Springmongoproject/src/main/java/com/example/Springmongoproject/Repo/DriverRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.Springmongoproject.Repo;
-
-import com.example.Springmongoproject.Entity.Driver;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DriverRepo extends MongoRepository<Driver, String> {
-}
--- Springmongoproject/src/main/java/com/example/Springmongoproject/Repo/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Springmongoproject.Repo;
-
-import com.example.Springmongoproject.Entity.Student;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepo extends MongoRepository<Student,String> {
-}
